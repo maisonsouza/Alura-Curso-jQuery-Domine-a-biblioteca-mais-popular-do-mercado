4b7d69cf7ab6ee568f2d57d65ce99f3506394a13
Maison Galvao <maisongalvao144@gmail.com>
2018-12-14 10:23:57 -0400

    Aula02 - Escutando eventos com Jquery

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,6 +1,13 @@
 var frase = $(".frase").text();
-var numpalavras = frase.split(" ").length;
+var numPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#quantidadepalavras");
-tamanhoFrase.text(numpalavras);
-console.log(frase);
-console.log(tamanhoFrase);
+tamanhoFrase.text(numPalavras);
+var campo = $(".campodigitacao");
+campo.on("input",function () {
+   var conteudo_digitado = campo.val();
+   var palavrasdigitadas = conteudo_digitado.split(/\S+/).length-1;
+   $('#contador-palavras').text(palavrasdigitadas);
+   var caracteresdigitados = conteudo_digitado.length;
+   $("#contador-caracteres").text(caracteresdigitados);
+});
+
